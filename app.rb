4f718d51397f7e9e1c456c81cfa61b63a475d809
greg1w <greg1w@hotmail.com>
2018-06-12 16:07:23 +0000

    Adding ruby code files.

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+class HelloWorld < Sinatra::Base
+
+    get '/' do
+        "Hello, world!"
+    end
+   
+    get '/:name' do
+        "Hello, #{params[:name]}!"
+    end
+end
